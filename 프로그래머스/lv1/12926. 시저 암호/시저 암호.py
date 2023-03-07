f0ee452b7bdb549e33ff2421ace3effae304c1de
믽 <106012542+mean-g@users.noreply.github.com>
2023-03-07 13:02:39 +0900

    [level 1] Title: 시저 암호, Time: 3.98 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,15 +1,24 @@
 def solution(s, n):
-    lower  = "abcdefghijklmnopqrstuvwxyz"
-    upper  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    answer = ""
+#     lower  = "abcdefghijklmnopqrstuvwxyz"
+#     upper  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#     answer = ""
     
+#     for i in s:
+#         if i in lower:
+#             j = lower.index(i) + n
+#             answer += lower[j % 26]
+#         elif i in upper:
+#             j = upper.index(i) + n
+#             answer += upper[j % 26]
+#         else:
+#             answer += " "
+#     return answer
+    answer = ''
     for i in s:
-        if i in lower:
-            j = lower.index(i) + n
-            answer += lower[j % 26]
-        elif i in upper:
-            j = upper.index(i) + n
-            answer += upper[j % 26]
+        if i >= 'A' and i <= 'Z':
+            answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
+        elif i >= 'a' and i <= 'z':
+            answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
         else:
-            answer += " "
+            answer += ' '
     return answer
