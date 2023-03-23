322e3b677226bb62c36d965b2d6cf856b3853ff0
믽 <106012542+mean-g@users.noreply.github.com>
2023-03-23 14:22:21 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,9 @@
+def solution(n, lost, reserve):
+    new_lo = set(lost) - set(reserve)
+    new_re = set(reserve) - set(lost)
+    for i in new_re:
+        if i - 1 in new_lo:
+            new_lo.remove(i - 1)
+        elif i + 1 in new_lo:
+            new_lo.remove(i + 1)
+    return n - len(new_lo)
