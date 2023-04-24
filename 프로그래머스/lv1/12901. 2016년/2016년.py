b67866571f0d69be0a84ba3b64b2267ab010ac52
믽 <106012542+mean-g@users.noreply.github.com>
2023-04-24 16:04:46 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,9 +1,15 @@
-def solution(a, b):
-    days   = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
-    months = [31,29,31,30,31,30,31,31,30,31,30,31]
-    d      = b - 1
+# def solution(a, b):
+#     days   = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+#     months = [31,29,31,30,31,30,31,31,30,31,30,31]
+#     d      = b - 1
     
-    for i in range(a-1):
-        d += months[i]
+#     for i in range(a-1):
+#         d += months[i]
         
-    return days[d % 7]
+#     return days[d % 7]
+
+import datetime
+
+def solution(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
