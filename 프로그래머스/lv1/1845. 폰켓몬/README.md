26e79afcaed825d4370e0941418de4992c68ffda
믽 <106012542+mean-g@users.noreply.github.com>
2023-05-15 14:43:21 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/README.md
+++ 프로그래머스/lv1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.40 ms
+메모리: 10.4 MB, 시간: 0.21 ms
 
 ### 구분
 
--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,12 @@
-def solution(nums):
-    answer = 0
+# def solution(nums):
+#     answer = 0
     
-    if len(set(nums)) >= len(nums) / 2:
-        answer = len(nums) / 2
-    else:
-        answer = len(set(nums))
+#     if len(set(nums)) >= len(nums) / 2:
+#         answer = len(nums) / 2
+#     else:
+#         answer = len(set(nums))
         
-    return answer
+#     return answer
+
+def solution(nums):
+    return min(len(nums) / 2, len(set(nums)))
