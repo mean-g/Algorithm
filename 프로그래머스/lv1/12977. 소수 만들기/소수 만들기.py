0143bbeb6d0270d3993283b2737fd65cdd8a8338
믽 <106012542+mean-g@users.noreply.github.com>
2023-05-22 14:04:01 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,33 +1,33 @@
-import itertools
-
-def solution(nums):
-    answer = 0
-    n_comb = list(itertools.combinations(nums, 3))
-    
-    for i in n_comb:
-        i = sum(i)
-        for j in range(2, i):
-            if i % j == 0:
-                break 
-        else:
-                answer += 1
-                
-    return answer
-
 # import itertools
-
-# def is_prime(n):
-#     for i in range(2, n):
-#         if n % i == 0:
-#             return False
-#     return True
 
 # def solution(nums):
 #     answer = 0
 #     n_comb = list(itertools.combinations(nums, 3))
-
-#     for j in n_comb:
-#         if is_prime(sum(j)):
+    
+#     for i in n_comb:
+#         i = sum(i)
+#         for j in range(2, i):
+#             if i % j == 0:
+#                 break 
+#         else:
 #             answer += 1
-
+                
 #     return answer
+
+import itertools
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    n_comb = list(itertools.combinations(nums, 3))
+
+    for j in n_comb:
+        if is_prime(sum(j)):
+            answer += 1
+
+    return answer
