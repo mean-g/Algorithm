1e8b48e78fb8ce8e6c165e7c19804f3c4993237a
믽 <106012542+mean-g@users.noreply.github.com>
2023-05-22 11:05:14 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,16 +1,30 @@
-def solution(k, score):
-    answer = []
-    honor  = []
+# def solution(k, score):
+#     answer = []
+#     honor  = []
     
-    for i in range(len(score)):
-        if len(honor) < k:
-            honor.append(score[i])
-            answer.append(min(honor))
-        elif score[i] > min(honor):
-            honor.append(score[i])
-            honor.remove(min(honor))
-            answer.append(min(honor))
-        else:
-            answer.append(min(honor))
+#     for i in range(len(score)):
+#         if len(honor) < k:
+#             honor.append(score[i])
+#             answer.append(min(honor))
+#         elif score[i] > min(honor):
+#             honor.append(score[i])
+#             honor.remove(min(honor))
+#             answer.append(min(honor))
+#         else:
+#             answer.append(min(honor))
             
+#     return answer
+
+def solution(k, score):
+
+    q = []
+
+    answer = []
+    for s in score:
+
+        q.append(s)
+        if (len(q) > k):
+            q.remove(min(q))
+        answer.append(min(q))
+
     return answer
