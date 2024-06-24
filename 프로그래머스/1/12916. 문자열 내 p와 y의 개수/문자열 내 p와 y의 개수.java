1456ae80815dce734a9356c76080ea8cdc27cc5a
믽 <106012542+mean-g@users.noreply.github.com>
2024-06-24 15:46:12 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,19 +1,19 @@
-// class Solution {
-//     boolean solution(String s) {
-//         int p = 0;
-//         int y = 0;
+class Solution {
+    boolean solution(String s) {
+        int p = 0;
+        int y = 0;
         
-//         for (int i = 0; i < s.length(); i++) {
-//             if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
-//                 p++;
-//             }
-//             else if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') { 
-//                 y++;
-//             }
-//         }
-//         return p == y;
-//     }
-// }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                p++;
+            }
+            else if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') { 
+                y++;
+            }
+        }
+        return p == y;
+    }
+}
 
 // class Solution {
 //     boolean solution(String s) {
@@ -31,17 +31,3 @@
 //         return p == y;
 //     }
 // }
-
-import java.util.stream.Collectors;
-
-class Solution {
-    boolean solution(String s) {
-        long p = s.chars()
-                  .filter(ch -> ch == 'p' || ch == 'P')
-                  .count();
-        long y = s.chars()
-                  .filter(ch -> ch == 'y' || ch == 'Y')
-                  .count();
-        return p == y;
-    }
-}
